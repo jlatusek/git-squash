c274349f5c8f2a69df40d6ba016e79499ed3a9e5
Jakub Latusek <jk.latusek@gmail.com>
2024-10-31 12:48:31 +0100

    Add arg parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::Parser;
+
+
+/// Squash current branch like GitHub squash merge
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of branch fromt whom squash
+    branch_name: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("args: {:?}", args);
 }
